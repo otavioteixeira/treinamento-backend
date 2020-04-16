1114ab1462c4b75cacde9ca7744af3875354c71f
otavio <otavio.otj@outlook.com>
2020-04-16 16:19:27 -0300

    Incluindo configurações maven

---
+++ Desafio2/src/main/java/br/com/zup/treinamento/models/Cliente.java
@@ -0,0 +1,59 @@
+package br.com.zup.treinamento.models;
+
+public class Cliente {
+
+    private String cpf;
+    private String nome;
+    private int idade;
+    private String email;
+    private String telefone;
+    private String endereço;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEndereço() {
+        return endereço;
+    }
+
+    public void setEndereço(String endereço) {
+        this.endereço = endereço;
+    }
+}
