50fda15b3cfe5d3cac74bc7e621a074498d4ae7b
otavio <otavio.otj@outlook.com>
2020-04-17 15:36:59 -0300

    Adicionando Projeto nivel 1 desafio 1

---
+++ desafios/src/test/java/br/com/zup/treinamento/desafios/DesafiosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.treinamento.desafios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
